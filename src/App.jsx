f478888b7911ecd4fb3ecdf11433b7309565f0f7
arbergosa <arbergosa@up.edu.ph>
2025-10-23 07:05:13 +0000

    refactor: move meta tags to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Toaster } from '@/components/ui/toaster';
@@ -18,6 +17,21 @@ function App() {
       <Helmet>
         <title>Explained PH JournCamp+ | National Campus Press Summit</title>
         <meta name="description" content="Join the Virtual Journalism Camp by Explained PH - a month-long program combining seasoned practitioners with award-winning storytellers to strengthen campus publications nationwide." />
+        <meta name="keywords" content="journalism, campus press, summit, workshop, philippines, explained ph, journcamp" />
+        <meta name="author" content="Explained PH" />
+
+        {/* Open Graph Meta Tags (for social media sharing) */}
+        <meta property="og:title" content="Explained PH JournCamp+ | National Campus Press Summit" />
+        <meta property="og:description" content="Join the Virtual Journalism Camp by Explained PH to strengthen campus publications nationwide." />
+        <meta property="og:image" content="https://i.postimg.cc/7Y0rb3Jh/journcamp.png" />
+        <meta property="og:url" content="https://journcamp.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Explained PH JournCamp+ | National Campus Press Summit" />
+        <meta name="twitter:description" content="Join the Virtual Journalism Camp by Explained PH to strengthen campus publications nationwide." />
+        <meta name="twitter:image" content="https://i.postimg.cc/7Y0rb3Jh/journcamp.png" />
       </Helmet>
       <div className="min-h-screen bg-slate-50 text-brand-dark">
         <Header />
